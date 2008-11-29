a4330dad08c49cc7c01cf016adf51886feb2034a
simian_mihnea@fotolitera.com <simian_mihnea@fotolitera.com@79a083ec-b03b-11dd-8830-69e4bb380a4a>
2008-11-29 12:08:58 +0000

    unimportant

--- constants.py
+++ constants.py
@@ -1,4 +1,6 @@
 sites = [ "CAUTCOLEG", "MAGAZINULDECASE"]
 
 url_matches = \
-{ "CAUTCOLEG" : ["http://cautcoleg.ro","http://www.cautcoleg.ro"] }
+{ "CAUTCOLEG" : ["http://cautcoleg.ro","http://www.cautcoleg.ro"] , \
+"MAGAZINULDECASE": ["http://magazinuldecase.ro", "http://www.magazinuldecase.ro"]
+}
